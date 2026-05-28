995118d53de719f3f1cf33a9fc7be6569827bfe3
신보영 <loopyrose363@gamil.com>
2026-05-28 17:18:11 +0900

    commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let c;
+let g = 20;
+
+let re_btn;
+let save_btn;
+
+
+let a = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1024,908);
+  background(10);
+  c = 0;
+  re_btn = createButton('RECREATE');
+  save_btn = createButton('SAVE');
 }
 
 function draw() {
-  background(220);
+  drawUI();
+
+  re_btn.mousePressed(recreate);
+  save_btn.mousePressed(saveimage);
+}
+
+
+function drawUI() {
+  fill(230);
+  rect(0,0,1024,140);
+
+  re_btn.size(g*5,g*5);
+  save_btn.size(g*5,g*5);
+  re_btn.position(g,g);
+  save_btn.position(g*7,g);
+}
+
+function recreate(){
+  background(255);
+}
+
+function saveimage () {
+  save(get(0,140,1024,768),"edit_Image"+a+".jpg");
+  a++;
 }
