847d65b640ac063e31e5e72f8d2c83a546bd5d76
신보영 <loopyrose363@gamil.com>
2026-06-02 14:41:47 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -4,10 +4,6 @@ let g = 20;
 
 let re_btn;
 let save_btn;
-let cam_btn;
-let pen_btn;
-let era_btn;
-
 let cam;
 
 let mode;
@@ -15,74 +11,48 @@ let a = 0;
 
 function setup() {
   createCanvas(1024,908);
-  background(255);
+  background(10);
   
   re_btn = createButton('RECREATE');
   save_btn = createButton('SAVE');
-  cam_btn = createButton('CAMERA')
-  pen_btn = createButton('PEN')
-  era_btn = createButton('ERASER')
-  cam = createCapture(VIDEO);
-  cam.hide();
+  // cam = createCapture(VIDEO);
+  // cam.hide();
+
   c = 0;
   sw = 1;
   mode = 0;
-
-  re_btn.mousePressed(recreate);
-  save_btn.mousePressed(saveimage);
-  cam_btn.mousePressed(cameraOn);
 }
 
 function draw() {
-
-  if (mode === 0 && mouseIsPressed) {
-      drawing();
-  } 
-  if (mode === 1 && mouseIsPressed) {
-      eraser()
-  } 
-  if (mode === 2 && mouseIsPressed) {
-      image(get(0,140,1024,768),0,140,1024,768);
-      mode = 0;
-      return;
-  }
   drawUI();
+
+  if (mode === 0) {
+    if (mouseIsPressed) {
+      drawing();
+    }
+  } else if (mode === 1) {
+    
+  } else if (mode === 2 ) {
+
+  }
+  re_btn.mousePressed(recreate);
+  save_btn.mousePressed(saveimage);
 }
 
 
 function drawUI() {
+  strokeWeight(1);
   fill(230);
   rect(0,0,1024,140);
 
-  //버튼 사이즈
   re_btn.size(g*5,g*5);
   save_btn.size(g*5,g*5);
-  cam_btn.size(g*5,g*5);
-  pen_btn.size(g*5,g*2);
-  era_btn.size(g*5,g*2);
   re_btn.position(g,g);
   save_btn.position(g*7,g);
-  cam_btn.position(g*13,g);
-  pen_btn.position(g*19,g);
-  era_btn.position(g*19,g*4);
-  
-  //색상 팔레트
-  fill();
-  rect();
 }
 
 function recreate() {
   background(255);
-}
-
-function saveimage () {
-  save(get(),"edit_Image"+a+".jpg");
-  a++;
-}
-
-function cameraOn() {
-  image(cam,0,140,1024,768)
-  mode = 2;
 }
 
 function drawing() {
@@ -91,8 +61,9 @@ function drawing() {
   line(mouseX,mouseY,pmouseX,pmouseY);
 }
 
-function eraser() {
-
+function saveimage () {
+  save(get(0,140,1024,768),"edit_Image"+a+".jpg");
+  a++;
 }
 
 function keyTyped() {
@@ -108,4 +79,3 @@ function keyTyped() {
     sw = 16
   }
 }
-
